068c2c9aaf89d76051a5dd08aba2a9c958b0c697
JayaKrishna <awaitingsravya@gmail.com>
2022-04-03 13:02:51 +0530

    MergeSort

--- com/SortingAlgos/Main.java
+++ com/SortingAlgos/Main.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static com.SortingAlgos.InsertionSort.insertion_Sort;
+import static com.SortingAlgos.MergeSort.mergeSort;
 import static com.SortingAlgos.QuickSort.quickSort;
 
 public class Main {
@@ -16,7 +17,8 @@ public class Main {
         ArrayList <Integer> inArray = new ArrayList<Integer>(List.of(2,4,3,6));
         System.out.println("Before Sorting: "+ inArray);
         //insertion_Sort(inArray);
-        quickSort(inArray);
+        //quickSort(inArray);
+        mergeSort(inArray);
         System.out.println("After Sorting: "+ inArray);
 
     }
